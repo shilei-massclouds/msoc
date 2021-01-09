a4500da927a9d6774f0d9e3e30261c08fc0498da
Shi Lei <shi_lei@massclouds.com>
2021-01-09 16:47:36 +0800

    add ip_fifo.sv

--- ip/ip_fifo.sv
+++ ip/ip_fifo.sv
@@ -12,12 +12,39 @@ module ip_fifo #(
 
     output  wire  empty,
     input   wire  rd_en,
-    output  wire  [(WIDTH-1):0] dout
+    output  reg   [(WIDTH-1):0] dout
 );
 
-    reg [(WIDTH-1):0] cells[(DEPTH-1):0];
+    localparam COUNT = 1 << DEPTH;
+
+    reg [(WIDTH-1):0] cells[COUNT];
 
     reg [(DEPTH-1):0] head;
     reg [(DEPTH-1):0] tail;
-    
+
+    assign empty = (tail == head);
+    assign full = (((tail + 1) % COUNT) == head);
+
+    always @(posedge rst) begin
+        if (rst) begin
+            head <= {DEPTH{1'b0}};
+            tail <= {DEPTH{1'b0}};
+            dout <= {WIDTH{1'b0}};
+        end
+    end
+
+    always @(posedge wr_clk, posedge rst) begin
+        if (~rst & wr_en) begin
+            cells[tail] <= din;
+            tail <= (tail + 1) % COUNT;
+        end
+    end
+
+    always @(posedge rd_clk, posedge rst) begin
+        if (~rst & rd_en) begin
+            dout <= cells[head];
+            head <= (head + 1) % COUNT;
+        end
+    end
+
 endmodule

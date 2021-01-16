e922974d4711ed9d7c7fa5ffa57020bc361d0261
Shi Lei <shi_lei@massclouds.com>
2021-01-16 14:02:50 +0800

    remove invalid state for ip232h

--- ip/ip_ft232h.sv
+++ ip/ip_ft232h.sv
@@ -13,12 +13,11 @@ module ip_ft232h (
 );
 
     localparam S_WRITE  = 2'b00;
-    localparam S_HANDLE = 2'b01;
-    localparam S_READ   = 2'b10;
+    localparam S_READ   = 2'b01;
 
     bit clk;
     bit [63:0] addr;
-    bit [2:0]  addr_off;
+    bit [3:0]  addr_cnt;
     bit [63:0] data;
     bit [3:0]  data_cnt;
 
@@ -39,21 +38,21 @@ module ip_ft232h (
     always @(posedge clk) begin
         if (state == S_WRITE) begin
             if (~wr_n) begin
-                $display($time,, "addr: %x %x", adbus, addr);
+                //$display($time,, "addr: %x %x %x", adbus, addr, addr_cnt);
                 addr <= {adbus, addr[63:8]};
-                addr_off <= addr_off + 1;
-                if (&addr_off) begin
-                    state <= S_HANDLE;
-                end
+                addr_cnt <= addr_cnt + 1;
             end
-        end else if (state == S_HANDLE) begin
-            i_txe_n <= 1'b1;
-            i_rxf_n <= 1'b0;
-            data <= addr;
-            state <= S_READ;
-        end else begin
+
+            if (addr_cnt[3]) begin
+                state <= S_READ;
+                i_txe_n <= 1'b1;
+                i_rxf_n <= 1'b0;
+                data <= addr;
+                addr_cnt <= 4'b0;
+            end
+        end else begin  /* state == S_READ */
             if (~oe_n & ~rd_n) begin
-                $display($time,, "data: %x %x", data, data_cnt);
+                //$display($time,, "data: %x %x", data, data_cnt);
                 adbus_buff <= data[7:0];
                 data <= {8'b0, data[63:8]};
                 if (data_cnt[3]) begin

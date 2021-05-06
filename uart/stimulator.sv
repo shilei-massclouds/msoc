aa8ba9d88f536c27e9d86da126a8cbbf5bf42689
Shi Lei <shi_lei@massclouds.com>
2021-05-06 17:17:10 +0800

    Add uart

---
+++ uart/stimulator.sv
@@ -0,0 +1,87 @@
+`timescale 1ns / 1ps
+
+`include "isa.vh"
+
+module stimulator (
+    input wire clk,
+    input wire rst_n,
+
+    tilelink.master bus
+);
+
+    localparam UART_RX_FIFO = 4'h0; /* In:  Recieve Buffer Register */
+    localparam UART_TX_FIFO = 4'h4; /* Out: Transmitter Holding Register */
+    localparam UART_STATUS  = 4'h8; /* In:  Line Status Register */
+    localparam UART_CONTROL = 4'hc; /* Out: Line Control Register */
+
+    localparam S_IDLE = 1'b0;
+    localparam S_ADDR = 1'b1;
+
+    reg  valid;
+    reg  [7:0] data;
+
+    /* Generator */
+    logic state, next_state;
+    dff dff_state(clk, rst_n, `DISABLE, `DISABLE, next_state, state);
+
+    assign bus.a_opcode  = `TL_GET;
+    assign bus.a_param   = 3'b0;
+    assign bus.a_size    = 3'd3;
+    assign bus.a_source  = 4'b0001;
+    assign bus.a_mask    = 8'hFF;
+    assign bus.a_corrupt = `FALSE;
+
+    assign bus.a_valid = (state == S_ADDR);
+    assign bus.d_ready = `ENABLE;
+
+    always @(rst_n, state, valid, bus.a_ready) begin
+        case (state)
+            S_IDLE:
+                next_state = valid ? S_ADDR : S_IDLE;
+            S_ADDR:
+                next_state = bus.a_ready ? S_IDLE : S_ADDR;
+            default:
+                next_state = S_IDLE;
+        endcase
+    end
+
+    always @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
+            bus.a_address <= 64'b0;
+        end else begin
+            if (state == S_IDLE && valid) begin
+                bus.a_address <= UART_TX_FIFO;
+                bus.a_data <= data;
+            end
+
+            if (state == S_ADDR && bus.a_ready)
+                bus.a_address <= 64'b0;
+        end
+    end
+
+    reg [7:0] count;
+    always @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
+            valid <= `ENABLE;
+            data <= "A";
+            count <= 8'b0;
+        end else begin
+            if (&count) begin
+                valid <= `ENABLE;
+                if (data >= "Z")
+                    data <= "A";
+                else
+                    data <= data;
+            end else begin
+                valid <= `DISABLE;
+            end
+
+            count <= count + 1;
+        end
+    end
+
+    initial begin
+        #10240 $finish();
+    end
+
+endmodule
---
+++ uart/stub/uart_stub.sv
@@ -0,0 +1,102 @@
+`timescale 1ns / 1ps
+
+`include "isa.vh"
+
+module uart (
+    input   wire    clk,
+    input   wire    rst_n,
+
+    tilelink.slave  bus
+);
+
+    localparam UART_RX_FIFO = 4'h0; /* In:  Recieve Buffer Register */
+    localparam UART_TX_FIFO = 4'h4; /* Out: Transmitter Holding Register */
+    localparam UART_STATUS  = 4'h8; /* In:  Line Status Register */
+    localparam UART_CONTROL = 4'hc; /* Out: Line Control Register */
+
+    //                          Status Register
+    // +--------+------+------+------+-------+-------+--------+-------+--------+
+    // |  31:8  |  7   |  6   |  5   |   4   |   3   |   2    |   1   |   0    |
+    // +--------+------+------+------+-------+-------+--------+-------+--------+
+    //  Reserved   PE     FE     OE   Intr-En Tx-Full Tx-Empty Rx-Full Rx-Valid
+    //
+    //  PE: Parity Error;   FE: Frame Error;    OE: Overrun Error;
+    //
+
+    //                          Control Register
+    //  +———————————+—————————————+———————————+——————————————+——————————————+
+    //  | 31:5      |     4       |  3:2      |      1       |      0       |
+    //  +———————————+—————————————+———————————+——————————————+——————————————+
+    //    Reserved    Enable Intr   Reserved    Rst Rx FIFO    Rst Tx FIFO
+    //
+
+    localparam UART_LSR_THRE = 8'b00100000; /* Transmit-hold-register empty */
+    localparam UART_LSR_TEMT = 8'b01000000; /* Transmitter empty */
+
+    localparam S_IDLE    = 3'b000;
+    localparam S_RX_FIFO = 3'b001;
+    localparam S_TX_FIFO = 3'b010;
+    localparam S_STATUS  = 3'b011;
+    localparam S_CONTROL = 3'b100;
+
+    reg [31:0] rx_buffer;
+    reg [31:0] status;
+    reg [31:0] control;
+
+    /* Controller */
+    logic [2:0] state, next_state;
+    dff #(3, 3'b0) dff_stage(clk, rst_n, `DISABLE, `DISABLE, next_state, state);
+
+    assign bus.a_ready = (state == S_IDLE);
+    assign bus.d_valid = (state == S_RX_FIFO) | (state == S_STATUS);
+    assign bus.d_data = ({32{state == S_RX_FIFO}} & rx_buffer) |
+                        ({32{state == S_STATUS}} & status);
+
+    /* State transition */
+    always @(state, bus.a_valid, bus.d_ready) begin
+        case (state)
+            S_IDLE:
+                if (bus.a_valid) begin
+                    if (bus.a_address == UART_RX_FIFO) begin
+                        next_state = S_RX_FIFO;
+                    end else if (bus.a_address == UART_TX_FIFO) begin
+                        next_state = S_TX_FIFO;
+                    end else if (bus.a_address == UART_STATUS) begin
+                        next_state = S_STATUS;
+                    end else if (bus.a_address == UART_CONTROL) begin
+                        next_state = S_CONTROL;
+                    end else begin
+                        next_state = S_IDLE;
+                    end
+                end else begin
+                    next_state = S_IDLE;
+                end
+            S_RX_FIFO:
+                next_state = bus.d_ready ? S_IDLE : S_RX_FIFO;
+            S_TX_FIFO:
+                next_state = S_IDLE;
+            S_STATUS:
+                next_state = bus.d_ready ? S_IDLE : S_STATUS;
+            S_CONTROL:
+                next_state = S_IDLE;
+            default:
+                next_state = S_IDLE;
+        endcase
+    end
+
+    /* Datapath */
+    always @(posedge clk, negedge rst_n) begin
+        if (~rst_n) begin
+            rx_buffer <= 32'b0;
+            status <= 32'b0;
+            control <= 32'b0;
+        end else begin
+            if (state == S_TX_FIFO) begin
+                $display("UART: %s", bus.a_data[7:0]);
+            end else if (state == S_CONTROL) begin
+                control <= bus.a_data[31:0];
+            end
+        end
+    end
+
+endmodule
